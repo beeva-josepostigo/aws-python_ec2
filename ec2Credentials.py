f344a7ee65843153f7d6468e266c03f261244054
Jose Manuel Postigo <jose.postigo@beeva.com>
2016-11-30 17:46:38 +0100

    Add ec2.py and ec2Credentials.py

---
+++ ec2Credentials.py
@@ -0,0 +1,13 @@
+import boto3
+import yaml
+
+def openSession():
+    with open("/home/josepostigo/amazon/ejemplo/config/credential.yaml","r") as credential_file:
+
+        credentials=yaml.load(credential_file)
+
+        session=boto3.Session(aws_access_key_id=credentials['config']['aws_access'],
+                       aws_secret_access_key=credentials['config']['aws_secret'])
+
+    return session
+
