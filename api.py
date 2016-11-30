a542689db194e7eb062864185338b60308a9dea8
Jose Manuel Postigo <jose.postigo@beeva.com>
2016-11-30 22:40:47 +0100

    Add comments in api.py

--- api.py
+++ api.py
@@ -4,27 +4,29 @@ from ejemplo.lib.ec2 import get_volumes
 from ejemplo.lib.ec2 import get_vpcs
 from ejemplo.lib.ec2 import put_tagInInstance
 
+#Get method to obtain name of instances defined in AWS account
 @get('/instances')
 def instances():
     instances=get_instances()
     return instances
 
+#Get method to obtain volumes defined in AWS account
 @get('/volumes')
 def volumes():
     volumes=get_volumes()
     return volumes
 
+#Get method to obtain VPCs defined in AWS account
 @get('/vpcs')
 def vpcs():
     vpcs=get_vpcs()
     return vpcs
 
-# @post('/login')
+#Post method to add tag 'Name' in a instance whose name is 'AlphaGroup'
 @route('/tag', method='POST')
 def tag():
     username = request.json['Username']
     put_tagInInstance(username)
-    # res= HTTPResponse(status=503,body="{}".format(username))
     return username
 
 run(host='localhost', port=8080, debug=True, reloader=True)
